ddd440a927ccd557a18cead9223d8a466e165bbe
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-14 16:34:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ hashketball = {
     team_name: "Brooklyn Nets",
     colors: ["Black", "White"],
     players: [
-      alan_anderson: {
+    {
         player_name: "Alan Anderson",
         number: 0,
         shoe: 16,
@@ -15,7 +15,7 @@ hashketball = {
         steals: 3,
         blocks: 1,
         slam_dunks: 1 },
-      reggie_evans: {
+      {
         player_name: "Reggie Evans",
         number: 30,
         shoe: 14,
@@ -25,7 +25,7 @@ hashketball = {
         steals: 12,
         blocks: 12,
         slam_dunks: 7 },
-      brook_lopez: {
+      {
         player_name: "Brook Lopez",
         number: 11,
         shoe: 17,
@@ -35,7 +35,7 @@ hashketball = {
         steals: 3,
         blocks: 1,
         slam_dunks: 15 },
-      mason_plumlee: {
+      {
         player_name: "Mason Plumlee",
         number: 1,
         shoe: 19,
@@ -45,7 +45,7 @@ hashketball = {
         steals: 3,
         blocks: 8,
         slam_dunks: 5 },
-      jason_terry: {
+      {
         player_name: "Jason Terry",
         number: 31,
         shoe: 15,
